fdf30bacfd54a6b9689bc540cb0b1cc0e4b78071
Sjoerd de Vries <sjdv1982@gmail.com>
2023-02-21 15:07:33 +0100

    update bash test with seamless.imperative changes

--- tests/bash.py
+++ tests/bash.py
@@ -12,7 +12,7 @@ executor_code = sctx.executor_code.value
 from seamless.imperative import _run_transformer, _get_semantic
 semantic_code_checksum = _get_semantic(executor_code, bytes.fromhex(executor_code_checksum))
 bashcode = '(ls $a; date) > RESULT'
-result = _run_transformer(
+result0 = _run_transformer(
     semantic_code_checksum, 
     executor_code_buffer, bytes.fromhex(executor_code_checksum), 
     signature=None, 
@@ -23,5 +23,16 @@ result = _run_transformer(
         "a": "/tmp",
         "pins_": ["a"],
     },
-)    
+    celltypes={
+        "a": "text",
+        "bashcode": "text",
+        "pins_": "plain",
+        "result": "bytes",
+    },
+    result_callback=None
+)
+try:
+    result = result0.decode()
+except Exception:
+    result = result0    
 print(result)
